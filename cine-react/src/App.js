4c6bc5262503a28a59cc523d60277de63a06aa90
Thibtoy <cabanes.thibault@gmail.com>
2019-05-06 16:07:00 +0200

    reglages

--- cine-react/src/App.js
+++ cine-react/src/App.js
@@ -4,18 +4,22 @@ import $ from 'jquery';
 import LandingPage from './LandingPage.js';
 import FindFilm from './FindFilm.js';
 import Footer from './Footer';
+
+const googleMap = require('@google/maps').createClient({
+  key: 'AIzaSyDWBYN_0J4Xx8RgudNX4sDqRZpnaTu9HKY'
+});
+
 class App extends Component {
 
   componentWillMount() {
     $.get('http://localhost:8000/api', function (data) {
-      console.log(data);
     })
   }
   render() {
     return (
       <div id="body">
-        <div id="Header"><h1 class="MasterFF">Header</h1></div>
-        <div id="Carrousel"><h1 class="MasterFF">Carrousel</h1></div>
+        <div id="Header"><h1 className="MasterFF">Header</h1></div>
+        <div id="Carrousel"><h1 className="MasterFF">Carrousel</h1></div>
         <LandingPage></LandingPage>
         <FindFilm></FindFilm>
 
