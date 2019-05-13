a7db4d70aabc2fc3037a157a1dff66c93c5e7ff5
Thibtoy <cabanes.thibault@gmail.com>
2019-05-13 14:54:35 +0200

    commit

--- API-ciné/src/Controller/ApiController.php
+++ API-ciné/src/Controller/ApiController.php
@@ -4,16 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+
 use App\Entity\Salle;
+use App\Entity\Reservations;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 header("Acces-Control-Allow-Origin: http://localhost:3000");
 
+/**
+ * @Route("/api", name="api")
+ */
 class ApiController extends AbstractController
 {
     /**
-     * @Route("/api", name="api")
+     * @Route("/", name="apiIndex")
      */
     public function index()
     {
@@ -25,8 +30,23 @@ class ApiController extends AbstractController
         }
 
         return new JsonResponse($data);
-       // return $this->render('api/index.html.twig', [
-            //'controller_name' => 'ApiController',
-        //]);
     }
+
+    /**
+     * @Route("/Reservation", name="apiResa")
+     */
+    public function Reservation()
+    {
+        $reservation = new Reservations();
+        $reservation->setMail('mail');
+        $reservation->setNom('nom');
+        $reservation->setPrenom('prénom');
+        $reservation->setSeanceFk('seance');
+        $reservation->setNotation('notation');
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($reservation);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('salle_index');
+    }   
 }
--- cine-react/src/App.js
+++ cine-react/src/App.js
@@ -5,14 +5,12 @@ import LandingPage from './LandingPage.js';
 import FindFilm from './FindFilm.js';
 import Footer from './Footer';
 
-const googleMap = require('@google/maps').createClient({
-  key: 'AIzaSyDWBYN_0J4Xx8RgudNX4sDqRZpnaTu9HKY'
-});
 
 class App extends Component {
 
   componentWillMount() {
-    $.get('http://localhost:8000/api', function (data) {
+    $.get('http://localhost:8000/api/', function (data) {
+    	console.log(data);
     })
   }
   render() {
