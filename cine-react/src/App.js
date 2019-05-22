a6c7d8f4df609129df915455c3adc21922c8fc95
HabibDi <habibdiallo78@gmail.com>
2019-05-22 15:10:15 +0200

    Router ajouté

--- cine-react/src/App.js
+++ cine-react/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './style.scss';
 import $ from 'jquery';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import LandingPage from './LandingPage.js';
 import FindFilm from './FindFilm.js';
 import Footer from './Footer';
 import Resa from './Resa';
+
+const ResaRoute = () => {
+  return (
+    <Resa></Resa>
+  );
+}
 
 class App extends Component {
 
@@ -19,10 +26,19 @@ class App extends Component {
         <div id="Header"><h1 className="MasterFF">Header</h1></div>
         <div id="Carrousel"><h1 className="MasterFF">Carrousel</h1></div>
         <LandingPage></LandingPage>
-        <Resa></Resa>
         <FindFilm></FindFilm>
         <Footer></Footer>
+
       </div>
+
+    );
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/resa" component={ResaRoute} />
+      </BrowserRouter>
     );
   }
 }
